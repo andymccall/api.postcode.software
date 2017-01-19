7f12f2c3e155580803befe3451eed27cd7dcf294
Andy McCall <mailme@andymccall.co.uk>
2017-01-19 22:23:39 +0000

    Initial commit

---
+++ src/software/postcode/api/Application.java
@@ -0,0 +1,16 @@
+/**
+ * Created by andymccall on 19/01/2017.
+ */
+
+package software.postcode.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/software/postcode/api/controller/RESTController.java
@@ -0,0 +1,24 @@
+/**
+ * Created by andymccall on 19/01/2017.
+ */
+
+package software.postcode.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class RESTController {
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET, produces="application/json")
+    public @ResponseBody String ping() {
+
+        return "pong";
+    }
+
+}
+
+
