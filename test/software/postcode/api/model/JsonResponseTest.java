e874d683fb084201e698910823310a11372d673e
Andy McCall <mailme@andymccall.co.uk>
2017-01-23 20:35:56 +0000

    Added unit tests for Ping and JsonResponse

---
+++ test/software/postcode/api/model/JsonResponseTest.java
@@ -0,0 +1,66 @@
+package software.postcode.api.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andymccall on 23/01/2017.
+ */
+public class JsonResponseTest {
+
+    // Constants
+    final public static int test1status = 200;
+    final public static String test1error = "Test error 1!";
+
+    final public static int test2status = 500;
+    final public static String test2error = "Test error 2!";
+
+
+    JsonResponse test1JsonResponse;
+    JsonResponse test2JsonResponse;
+
+    @Before
+    public void setUp() throws Exception {
+        test1JsonResponse = new JsonResponse() {
+            @Override
+            public String toString() {
+                return null;
+            }
+        };
+        test1JsonResponse.setStatus(test1status);
+        test1JsonResponse.setError(test1error);
+
+        test2JsonResponse = new JsonResponse() {
+            @Override
+            public String toString() {
+                return null;
+            }
+        };
+    }
+
+    @Test
+    public void getStatus() throws Exception {
+        Assert.assertEquals(test1JsonResponse.getStatus(), test1status);
+    }
+
+    @Test
+    public void getError() throws Exception {
+        Assert.assertEquals(test1JsonResponse.getError(), test1error);
+    }
+
+    @Test
+    public void setStatus() throws Exception {
+        test2JsonResponse.setStatus(test2status);
+        Assert.assertEquals(test2JsonResponse.getStatus(), test2status);
+    }
+
+    @Test
+    public void setError() throws Exception {
+        test2JsonResponse.setError(test2error);
+        Assert.assertEquals(test2JsonResponse.getError(), test2error);
+    }
+
+}
---
+++ test/software/postcode/api/model/PingTest.java
@@ -0,0 +1,32 @@
+package software.postcode.api.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andymccall on 23/01/2017.
+ */
+public class PingTest {
+
+    Ping testPing;
+
+    @Before
+    public void setUp() throws Exception {
+        testPing = new Ping() {
+            @Override
+            public String toString() {
+                return null;
+            }
+        };
+    }
+
+    @Test
+    public void getResponse() throws Exception {
+        Assert.assertEquals(testPing.getResponse(), "pong");
+    }
+
+
+}
