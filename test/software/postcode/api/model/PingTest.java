df3310080a0d6f03d15625aeccaa321f343ae1a0
Andy McCall <mailme@andymccall.co.uk>
2017-01-24 21:51:56 +0000

    Completed JavaDoc for PingTest

--- test/software/postcode/api/model/PingTest.java
+++ test/software/postcode/api/model/PingTest.java
@@ -4,15 +4,21 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
- * Created by andymccall on 23/01/2017.
+ * The PingTest class is a test class that tests Ping.
+ *
+ * @author  Andy McCall
+ * @version 0.1
+ * @since   2017-01-24
  */
 public class PingTest {
 
     private Ping testPing;
 
+    /**
+     * Sets up objects and mocks external dependencies needed
+     * for the tests.
+     */
     @Before
     public void setUp() throws Exception {
         testPing = new Ping() {
@@ -23,10 +29,12 @@ public class PingTest {
         };
     }
 
+    /**
+     * Tests Ping.getResponse()
+     */
     @Test
     public void getResponse() throws Exception {
         Assert.assertEquals(testPing.getResponse(), "pong");
     }
-
 
 }
